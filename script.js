5a20052a81eb5655daa956d746d2a044dd48eefd
manish021124 <maneeshbalami@gmail.com>
2022-07-30 12:25:21 +0545

    update

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let list = document.getElementById('list');
 document.getElementById('btn').addEventListener("click", show);
 document.getElementById('btn1').addEventListener("click", unshow);
 
-
 function show(){
     list.style.visibility = "visible";
 }
