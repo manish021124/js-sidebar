288d4ed30f7167cf9075fb0543901e7cc565026d
manish021124 <maneeshbalami@gmail.com>
2022-07-31 14:36:03 +0545

    update

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 let list = document.getElementById('list');
 
 document.getElementById('btn').addEventListener("click", show);
-document.getElementById('btn1').addEventListener("click", unshow);
+document.getElementById('btn').addEventListener("click", anime);
+document.getElementById('btn1').addEventListener("click", reverseAnime);
 
 function show(){
     list.style.visibility = "visible";
 }
 
-function unshow(){
-    list.style.visibility = "hidden";
+function anime(){
+    let pos = -480;
+    let id = null;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame(){
+        if(pos == 0){
+            clearInterval(id);
+        }else{
+            pos++;
+            list.style.left = pos + "px";
+        }
+    }
 }
 
-
+function reverseAnime(){
+    let pos = 0;
+    let id = null;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame(){
+        if(pos == (-480)){
+            clearInterval(id);
+        }else{
+            pos--;
+            list.style.left = pos + "px";
+        }
+    }
+}
