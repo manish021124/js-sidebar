c42bba101652261a2abd6d359add365637d413d4
Manish Balami <89579663+manish021124@users.noreply.github.com>
2022-09-06 18:47:58 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
-let list = document.getElementById('list');
-
 document.getElementById('btn').addEventListener("click", show);
-document.getElementById('btn').addEventListener("click", anime);
-document.getElementById('btn1').addEventListener("click", reverseAnime);
+document.getElementById('btn1').addEventListener("click", hide);
 
 function show(){
-    list.style.visibility = "visible";
+    document.getElementById('list').style.width = "30%";
 }
 
-function anime(){
-    let pos = -480;
-    let id = null;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame(){
-        if(pos == 0){
-            clearInterval(id);
-        }else{
-            pos++;
-            list.style.left = pos + "px";
-        }
-    }
-}
-
-function reverseAnime(){
-    let pos = 0;
-    let id = null;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame(){
-        if(pos == (-480)){
-            clearInterval(id);
-        }else{
-            pos--;
-            list.style.left = pos + "px";
-        }
-    }
+function hide(){
+    document.getElementById('list').style.width = "0";
 }
